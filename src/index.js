e6aa633763d06dca6e35772ac767ef39e0e28bbb
IvBrt <crstn.b7@gmail.com>
2022-01-24 10:49:34 -0600

    se realizó el objeto isValid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const box = document.getElementById("numero");
+
+const send = document.getElementById("enviar");
+send.addEventListener("click", getNumber);
+
+function getNumber() {
+ const number = box.value;
+ console.log(number); 
+ validator.isValid(number);
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,9 @@
 const validator = {
-  // ...
+  isValid: function(numero) {
+    const cardNumber = Array.from(numero).reverse();
+    console.log(cardNumber);
+
+  }
 };
 
 export default validator;
