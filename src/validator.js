6d610a9ea2419dd1f6788f0c5a2925ac70848cbf
IvBrt <crstn.b7@gmail.com>
2022-01-27 13:34:43 -0600

    obtuve la suma de los numeros

--- src/validator.js
+++ src/validator.js
@@ -3,6 +3,21 @@ const validator = {
     const cardNumber = Array.from(numero).reverse();
     console.log(cardNumber);
 
+    for(let i=0; i<16; i++){
+      let non =[i];
+      if(non%2===1){
+        let doubles = cardNumber[i] * 2;  
+        if(doubles > 9){
+          let numberAdd = doubles.toString().split("");
+          numberAdd = parseInt(numberAdd[0])+ parseInt(numberAdd[1]);
+          console.log(numberAdd);
+
+        }
+        
+      }
+      
+    }
+
   }
 };
 
