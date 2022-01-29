1ac9cc1af352c7834fd4fe72c90d415ab7d5183a
IvBrt <crstn.b7@gmail.com>
2022-01-29 00:37:36 -0600

    suma total

--- src/validator.js
+++ src/validator.js
@@ -1,24 +1,39 @@
 const validator = {
-  isValid: function(numero) {
-    const cardNumber = Array.from(numero).reverse();
+  isValid: function (numero) {
+    let cardNumber = Array.from(numero).reverse();
+    var total = 0; // declaramos variable en 0
+
     console.log(cardNumber);
 
-    for(let i=0; i<16; i++){
-      let non =[i];
-      if(non%2===1){
-        let doubles = cardNumber[i] * 2;  
-        if(doubles > 9){
-          let numberAdd = doubles.toString().split("");
-          numberAdd = parseInt(numberAdd[0])+ parseInt(numberAdd[1]);
-          console.log(numberAdd);
-
+    for (let i = 0; i < cardNumber.length; i++) {
+      let non = i; // le asignmos non la posicon i = 0, i= 1, i=2 , etc en cada iteracion
+      let doubles;
+      if (non % 2 === 1) {
+        // verificamos si es impar con el residuo
+        doubles = cardNumber[i] * 2; // le asignamos a doubles el valor de cardNumber[posicion]
+        console.log(doubles);
+        if (doubles > 9) {
+          //si doubles es mayor a 9 entra
+          let numberAdd = doubles.toString().split(""); // separamos el valor de dobles
+          numberAdd = parseInt(numberAdd[0]) + parseInt(numberAdd[1]); // sumamos los valores separados
+          cardNumber[i] = numberAdd; // sustituimos el valor de cardNumber[posicion] por sumatoria de (numAdd[0]+numAdd[1])
+        } else {
+          // si no
+          cardNumber[i] = doubles; //sustituimos el valor de cardNumber[posicion] por el valor de doubles
         }
-        
       }
+    } //aquí termina for
+    console.log(cardNumber);
+
+    for (let i = 0; i < cardNumber.length; i++) {
+      // iteramos para sumar
+      let intNum = parseInt(cardNumber[i]); //declaamos intNym y le asignamos el valor de cardNumber[posicion]
+
+      total += intNum; // le sumamos intNum a total
       
     }
-
-  }
+    console.log("this total: " + total);
+  },
 };
 
 export default validator;
